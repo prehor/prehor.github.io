1ff908c85080aa6fdc7e5fecef9a70d993037de0
prehor <prehor@users.noreply.github.com>
2021-09-28 21:36:47 +0000

    jekyll build from Action 4a57fb85c537ebb00cf1fefbc812b35d9d9db960

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "Welcome!",
+        "excerpt":"Hello world, this is my first blog post.   I hope you like it!  ","categories": ["posts"],
+        "tags": [],
+        "url": "/posts/2020/03/22/welcome.html",
+        "teaser": null
+      },{
+        "title": "Cmd-+ on Czech Keyboard",
+        "excerpt":"The MacBook Czech keyboard (and probably other European/ISO keyboards as well) lacks the Cmd-+ key used for Zoom In in most applications.   Here is a complex modification for Karabiner-Elements that maps Cmd-&lt; key (at the left of key 1) to Cmd-+:      Download the file command-+-czech.json to the ~/.config/karabiner/assets/complex_modifications folder.   In the Karabiner-Elements preferences go to the Complex modifications menu.   Click Add rule and  enable rule in Command-+ on Czech (European/ISO) Keyboard section.   Enjoy Cmd-+ with Cmd-&lt; key (at the left of key 1).  ","categories": ["posts"],
+        "tags": ["Apple","macOS"],
+        "url": "/posts/2020/03/26/cmd-plus-on-czech-keyboard.html",
+        "teaser": null
+      },{
+        "title": "Microsoft Remote Desktop with Czech Keyboard",
+        "excerpt":"Karabiner-Elements mappings (control keys and czech language keys) that allow you to use the Apple macOS Czech keyboard layout in Windows when connected using Microsoft Remote Desktop.      Download the file microsoft-remote-desktop.json and microsoft-remote-desktop-czech.json to the ~/.config/karabiner/assets/complex_modifications folder.   In the Karabiner-Elements preferences go to the Complex modifications menu.   Click Add rule and  enable rules in Microsoft Remote Desktop sections.   Enjoy Apple macOS czech keyboard in Windows remote desktop.  ","categories": ["posts"],
+        "tags": ["Apple","macOS","Remote Desktop","Windows"],
+        "url": "/posts/2020/03/30/remote-desktop-with-czech-keyboard.html",
+        "teaser": null
+      }]
